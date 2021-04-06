8c17b3901b469a41341fbf4b67588a75471975cc
Joey-Van <fjy18013265103@163.com>
2021-04-06 23:18:52 +0800

    first commit

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,3 @@
+public interface UserDao {
+    User select(User user);
+}
---
+++ src/main/java/service/UserServicelmpl.java
@@ -0,0 +1,12 @@
+package service;
+
+public class User {
+    private String username;
+    private String password;
+    public User(String username, String password){
+        this.username=username;
+        this.password=password;
+
+    }
+    //省略getter and setter
+}
